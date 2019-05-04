e959b3c45660729f53ed7dbf8c0f12aa436a3001
NicolasJandard <n.jandard@gmail.com>
2019-05-04 14:30:38 +0200

    fix environment url heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://rencontr-esport-develop-back.herokuapp.com/'
+  baseUrl: 'https://rencontr-esport-develop-back.herokuapp.com'
 };
